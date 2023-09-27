4efa21323e172630e88ccc65fe5d9da245c0395a
dave <davidgriffiths@hotmail.com>
2023-09-27 12:07:58 +0100

    Case Page and journal enrty component boilerplate

--- resources/js/Pages/Concern/Case.vue
+++ resources/js/Pages/Concern/Case.vue
@@ -4,8 +4,9 @@ import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { useForm, Head } from '@inertiajs/vue3';
 import Issue from '@/Components/Issue.vue';
+import JournalEntry from '@/Components/Issue.vue';
 
-defineProps(['concerns']);
+defineProps(['concern']);
  
 </script>
  
@@ -16,9 +17,15 @@ defineProps(['concerns']);
         <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
             <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
                 <Issue
-                    v-for="concern in concerns"
                     :key="concern.id"
                     :concern="concern"
+                />
+            </div>
+            <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                <JournalEntry
+                    v-for="entry in concern.journalEntries"
+                    :key="entry.id"
+                    :entry="entry"
                 />
             </div>
         </div>
